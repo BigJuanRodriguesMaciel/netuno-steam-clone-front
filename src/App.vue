d089a51ff6bcf50fed192928ad857ad6e3b9577a
Juan Rodrigues Maciel <juan@fasters.com.br>
2022-09-29 16:12:01 -0300

    feat(App) bootstrap implementing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <b-container class="bv-example-row">
     <b-row>
-      <b-col md="1">1 of 3</b-col>
-      <b-col md="3">2 of 3</b-col>
-      <b-col md="7">3 of 3</b-col>
+      <b-col md="3" v-for="(item, index) in imgDatas" :key="index">
+        <MainHeader :image="item" />
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-  export default {};
+  import MainHeader from "./components/MainHeader.vue";
+  export default {
+    data() {
+      return {
+        imgDatas: [
+          "https://loremflickr.com/640/480",
+          "https://loremflickr.com/640/480",
+          "https://loremflickr.com/640/480",
+          "https://loremflickr.com/640/480",
+          "https://loremflickr.com/640/480",
+        ],
+      };
+    },
+    components: { MainHeader },
+  };
 </script>
